47f77558757418c9e11c0e5090200e62c3a91a18
radishengine <radishengine@gmail.com>
2017-07-05 23:10:10 +0100

    Update iagarden_main.js

--- iagarden_main.js
+++ iagarden_main.js
@@ -3,11 +3,11 @@ requirejs(['domReady!', 'ia'], function(domReady, ia) {
   'use strict';
   
   function loadHash() {
-    var hash = (location.hash || '').match(/^#?\/?([a-zA-Z0-9_-\.]+)\/?(.*)$/);
+    var hash = (location.hash || '').match(/^#?\/?([a-zA-Z0-9_\-\.]+)\/?(.*)$/);
     if (!hash) {
       hash = localStorage.get('lastValidHash') || '#/amigaformat045disk_1993-04/';
       history.replaceState(undefined, undefined, hash);
-      hash = hash.match(/^#?\/?([a-zA-Z0-9_-\.]+)\/?(.*)$/);
+      hash = hash.match(/^#?\/?([a-zA-Z0-9_\-\.]+)\/?(.*)$/);
     }
     localStorage.set('lastValidHash', '#/' + hash[1] + '/' + hash[2]);
     var itemName = hash[1];
