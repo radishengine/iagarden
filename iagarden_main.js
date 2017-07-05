cf965aa2235156d59fb7ba75d685591946f51020
radishengine <radishengine@gmail.com>
2017-07-05 20:42:45 +0100

    Update iagarden_main.js

--- iagarden_main.js
+++ iagarden_main.js
@@ -1,9 +1,24 @@
-requirejs(['domReady'], function() {
+requirejs(['domReady', 'ia'], function(domReady, ia) {
 
   'use strict';
   
   var itemElements = document.querySelectorAll('[data-ia-item]');
   
+  function initItemElement(el) {
+    ia.fileList(el.dataset.iaItem)
+    .then(function(files) {
+      files.forEach(function(fileInfo) {
+        var element = document.createElement('DIV');
+        element.className = 'file';
+        Object.assign(element.dataset, fileInfo);
+        element.innerText = fileInfo.name;
+        el.appendChild(element);
+      });
+    });
+  }
   
+  for (var i = 0; i < itemElements.length; i++) {
+    initItemElement(itemElements[i]);
+  }
 
 });
