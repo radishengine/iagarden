7ef3d987c277c8acdf2522999b22ec228351d451
radishengine <radishengine@gmail.com>
2017-07-05 22:18:22 +0100

    Update iagarden_main.js

--- iagarden_main.js
+++ iagarden_main.js
@@ -9,12 +9,8 @@ requirejs(['domReady!', 'ia'], function(domReady, ia) {
     .then(function(files) {
       files.forEach(function(fileInfo) {
         var element = document.createElement('A');
-        if (fileInfo.format === 'JPEG') {
-          element.setAttribute('href', ia.getLinkURL(el.dataset.iaItem, fileInfo.name));
-        }
-        else {
-          element.setAttribute('href', '#' + el.dataset.iaItem + '/' + fileInfo.name);
-        }
+        element.setAttribute('href', ia.getLinkURL(el.dataset.iaItem, fileInfo.name));
+        // element.setAttribute('href', '#/' + el.dataset.iaItem + '/' + fileInfo.name);
         element.className = 'file';
         Object.assign(element.dataset, fileInfo);
         element.innerText = fileInfo.name;
