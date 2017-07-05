0967ef224fde6e21aca0c7381202c02d4298817b
radishengine <radishengine@gmail.com>
2017-07-05 23:05:56 +0100

    Update iagarden_main.js

--- iagarden_main.js
+++ iagarden_main.js
@@ -2,25 +2,29 @@ requirejs(['domReady!', 'ia'], function(domReady, ia) {
 
   'use strict';
   
-  var itemElements = document.querySelectorAll('[data-ia-item]');
-  
-  function initItemElement(el) {
-    ia.fetchFileList(el.dataset.iaItem)
-    .then(function(files) {
+  function loadHash() {
+    var hash = (location.hash || '').match(/^#?\/?([a-zA-Z0-9_-\.]+)\/?(.*)$/);
+    if (!hash) {
+      hash = localStorage.get('lastValidHash') || '#/amigaformat045disk_1993-04/');
+      history.replaceState(hash);
+      hash = (location.hash || '').match(/^#?\/?([a-zA-Z0-9_-\.]+)\/?(.*)$/);
+    }
+    localStorage.set('lastValidHash', '#/' + hash[1] + '/' + hash[2]);
+    var itemName = hash[1];
+    var fileContainer = document.getElementById('files');
+    ia.getFileList(itemName).then(function(files) {
       files.forEach(function(fileInfo) {
         var element = document.createElement('A');
-        element.setAttribute('href', ia.getLinkURL(el.dataset.iaItem, fileInfo.name));
-        // element.setAttribute('href', '#/' + el.dataset.iaItem + '/' + fileInfo.name);
+        element.setAttribute('href', ia.getLinkURL(itemName, fileInfo.name));
+        // element.setAttribute('href', '#/' + itemName + '/' + fileInfo.name);
         element.className = 'file';
         Object.assign(element.dataset, fileInfo);
         element.innerText = fileInfo.name;
-        el.appendChild(element);
+        fileContainer.appendChild(element);
       });
     });
   }
+  window.addEventListener('hashchange', loadHash);
+  loadHash();
   
-  for (var i = 0; i < itemElements.length; i++) {
-    initItemElement(itemElements[i]);
-  }
-
 });
